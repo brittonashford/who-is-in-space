b94a3d67fb2d05c17171bd00826c9ca7336ba6a4
brittonashford <brittonashford@users.noreply.github.com>
2021-12-25 22:14:57 -0600

    replace getJSON() with fetch() add error handling added "craft" property & HTML

--- js/promises.js
+++ js/promises.js
@@ -3,58 +3,52 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = () => {
-      if(xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        resolve(data);
-      } else {
-        reject( Error(xhr.statusText) );
-      }
-    };
-    xhr.onerror = () => reject( Error('A network error occurred') );
-    xhr.send();
-  });
-}
-
 function getProfiles(json) {
-  const profiles = json.people.map( person => {
-    return getJSON(wikiUrl + person.name);      
-  }); 
-  return Promise.all(profiles);
+    const profiles = json.people.map(person => {
+        const craft = person.craft;
+        return fetch(wikiUrl + person.name)
+            .then(response => response.json())
+            .then(profile => {
+                return { ...profile, craft };
+            })
+            .catch(err => console.log('Error Fetching Wiki: ', err))
+    });
+    return Promise.all(profiles);
 }
 
 function generateHTML(data) {
-  data.map( person => {
-    const section = document.createElement('section');
-    peopleList.appendChild(section);
-    // Check if request returns a 'standard' page from Wiki
-    if (person.type === 'standard') {
-      section.innerHTML = `
+    data.map(person => {
+        const section = document.createElement('section');
+        peopleList.appendChild(section);
+        // Check if request returns a 'standard' page from Wiki
+        if (person.type === 'standard') {
+            section.innerHTML = `
         <img src=${person.thumbnail.source}>
         <h2>${person.title}</h2>
         <p>${person.description}</p>
         <p>${person.extract}</p>
       `;
-    } else {
-      section.innerHTML = `
+        } else {
+            section.innerHTML = `
         <img src="img/profile.jpg" alt="ocean clouds seen from space">
         <h2>${person.title}</h2>
         <p>Results unavailable for ${person.title}</p>
         ${person.extract_html}
       `;
-    }
-  });
+        }
+    });
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl)
-    .then(getProfiles)
-    .then(generateHTML)
-    .catch( err => console.log(err) )
+    event.target.textContent = "Loading...";
 
-  event.target.remove() 
+    fetch(astrosUrl)
+        .then(response => response.json())
+        .then(getProfiles)
+        .then(generateHTML)
+        .catch(err => {
+            peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+            console.log(err);
+        })
+        .finally(() => event.target.remove())
 });
